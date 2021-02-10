ed8ebd6d6d1e762c0fddf1f253c2c1dac8e8cbcc
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2021-02-09 22:18:30 -0500

    add dirty check to build.rs

--- build.rs
+++ build.rs
@@ -11,4 +11,5 @@ fn main() {
     let url = "https://github.com/evansiroky/timezone-boundary-builder/releases/download/2020d/timezones.shapefile.zip";
     check_command(Command::new("wget").current_dir("tzdata").arg(url).arg("-O").arg("timezones.zip").status(), "failed to download timezone file");
     check_command(Command::new("unzip").current_dir("tzdata").arg("-o").arg("timezones.zip").status(), "failed to unzip");
+    println!("cargo:rerun-if-changed=build.rs");
 }
